1b46cdba00d4e9055c0669538eeeca5d020fa59d
Richard Beattie <richard.beattie91@gmail.com>
2023-07-07 17:23:09 -0400

    Use btmon

--- spherov2/adapter/bleak_adapter.py
+++ spherov2/adapter/bleak_adapter.py
@@ -1,21 +1,48 @@
 from bleak import BleakScanner, BleakClient
 from bleak.backends.bluezdbus import defs, utils
 from dbus_fast.message import Message
-import sys
+# import sys
 import asyncio
-import pdb
+# import pdb
 import inspect
 import functools
-import os
+# import os
 from spherov2.socketfromfd import fromfd
-import pyshark, threading, pdb
-import queue
+# import pyshark, 
+import threading
+# import queue
 import multiprocessing
+import logging
+# import time
+import subprocess
+
+logpath = "sphero_log.log"
+logger = logging.getLogger('log')
+logger.setLevel(logging.INFO)
+ch = logging.FileHandler(logpath)
+ch.setFormatter(logging.Formatter('%(message)s'))
+logger.addHandler(ch)
+
+def get_bytearray_from_hex_string(hex_string: str) -> bytearray:
+    # Split the string into individual hex values
+    hex_values = hex_string.split(b":")
+
+    # Convert hex values to integers
+    int_values = [int(hex_value, 16) for hex_value in hex_values]
+
+    # Create a bytearray from the integer values
+    return bytearray(int_values)
 
 class Sniffer:
     _sniffer_process = None
     _queue = None
     _capturing_event = None
+    _listeners = {
+        "hci0": {},
+        "hci1": {},
+        "hci2": {},
+    }
+    _handles_to_address = {}
 
     @staticmethod
     def init():
@@ -33,20 +60,58 @@ class Sniffer:
 
     @staticmethod
     def capture_live_packets():
-        print("Capturing in {id}!".format(id=multiprocessing.current_process().pid))
-        capture = pyshark.LiveCapture(interface="bluetooth0")
-        packet_generator = capture.sniff_continuously()
-        # pyshark needs to start before any ble connections are made
+        # Create btmon processes
+        btmon_process = subprocess.Popen(['btmon'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         Sniffer._capturing_event.set()
-        for raw_packet in packet_generator:
-            if 'bthci_acl' in raw_packet:
-                if 'src_bd_addr' in raw_packet.bthci_acl.field_names:
-                    if raw_packet.bthci_acl.src_bd_addr in ['d1:b2:09:68:f8:60', 'c6:e7:40:7b:1b:35']:
-                        if 'btatt' in raw_packet and raw_packet.btatt.get('opcode') == '0x0000001b':
-                            if raw_packet.btatt.get('service_uuid128') == '00:01:00:01:57:4f:4f:20:53:70:68:65:72:6f:21:21':
-                                bt_bytes = bytearray(raw_packet.btatt.get('value').encode('latin1'))
-                                # print("added to queue")
-                                Sniffer._queue.put(bt_bytes)
+        print("Capturing!")
+
+        i = 0
+        mode = 0
+        handle_to_add = None
+        for stdout_line in iter(btmon_process.stdout.readline, ""):
+            if i == 0:
+                if b"ATT: Handle Value Notification" in stdout_line:
+                    i = 2
+                    mode = 1
+                if b"LE Connection Complete" in stdout_line:
+                    i = 2
+                    mode = 2
+            else:
+                i -= 1
+                if i == 0:
+                    if mode == 1:
+                        data = stdout_line.split(b"Data: ")[1].strip()
+                        bt_bytes = bytearray.fromhex(data.decode('utf-8'))
+                        Sniffer._queue.put(bt_bytes)
+                        mode = 0
+                    elif mode == 2:
+                        handle_to_add = stdout_line.split(b"Handle: ")[1].strip()
+                        mode = 3 
+                        i = 3
+                    elif mode == 3:
+                        data = stdout_line.split(b"Peer address: ")[1].strip()
+                        data = data.split(b" ")[0].strip()
+                        Sniffer._handles_to_address[handle_to_add] = data
+                        print(Sniffer._handles_to_address)
+                        mode = 0
+        # print("Capturing in {id}!".format(id=multiprocessing.current_process().pid))
+        # logger.info("Started Capturing")
+        # capture = pyshark.LiveCapture(interface="bluetooth0")
+        # packet_generator = capture.sniff_continuously()
+        # # pyshark needs to start before any ble connections are made
+        # Sniffer._capturing_event.set()
+        # logger.info("Set the event")
+        # for raw_packet in packet_generator:
+        #     if 'btatt' in raw_packet and raw_packet.btatt.get('opcode') == '0x0000001b':
+        #         if raw_packet.btatt.get('service_uuid128') == '00:01:00:01:57:4f:4f:20:53:70:68:65:72:6f:21:21':
+        #             if 'bthci_acl' in raw_packet:
+        #                 if 'src_bd_addr' in raw_packet.bthci_acl.field_names:
+        #                     if raw_packet.bthci_acl.src_bd_addr in ['d1:b2:09:68:f8:60', 'c6:e7:40:7b:1b:35']:
+        #                         bt_bytes = get_bytearray_from_hex_string(raw_packet.btatt.get('value').encode('latin1'))
+        #                         # print("added to queue")
+        #                         with open('second_log.log', 'a') as file:
+        #                             file.write("Packet captured at {time} with value = {value}\n".format(time=time.time(), value=','.join('{:02x}'.format(x) for x in bt_bytes)))
+        #                         Sniffer._queue.put(bt_bytes)
 
 class BleakAdapter(BleakClient):
 
@@ -99,7 +164,7 @@ class BleakAdapter(BleakClient):
 
     def handle_follow_file_task_done(self, task):
         print("Tasked ended!")
-        pdb.set_trace()
+        # pdb.set_trace()
         # quit()
 
     # async def set_callback(self, uuid, callback):
@@ -110,56 +175,11 @@ class BleakAdapter(BleakClient):
         while True:
             await asyncio.sleep(0)
             try:
-                # print("getting packet")
                 packet = Sniffer._queue.get_nowait()
-                print(packet)
+                # print(packet)
+                callback(packet)
             except: 
-                # print("Exception!")
                 continue
-
-            # print(packet)
-            # try:
-            #     # packet = queue.get_nowait()
-            #     # packet = test_queue.get_nowait()
-            #     pass
-            #     # packet = Sniffer._queue.get_nowait()
-            #     # print(packet)
-            #     # raise ValueError("Something")
-            # except Exception as e:
-            #     continue
-            # print("There are {x} elements in queue".format(x=Sniffer._queue.qsize())) 
-            # try:
-            #     # packet = queue.get_nowait()
-            #     packet = Sniffer._queue.get()
-            #     print(packet)
-            #     # raise ValueError("Something")
-            # except Exception as e:
-            #     continue
-                
-            # print("got a packet!")
-                # pdb.set_trace()
-                # print("Exception!")
-
-            # await asyncio.sleep(0)
-        #     try:
-        #         packet = Sniffer.get_queue_item()
-        #         # print("Got packet!")
-        #         print(packet)
-        #     except:
-        #         # print("Error while getting packet")
-        #         await asyncio.sleep(0)
-        #         continue
-            
-        #     if packet is None:
-        #         pass
-
-        #     print("got packet from queue")
-
-        #     callback(packet)
-
-    # @staticmethod
-    # def test_callback(uuid, data):
-    #     print(data)
 
     async def set_callback(self, uuid, callback):
         """
@@ -176,10 +196,10 @@ class BleakAdapter(BleakClient):
         else:
             wrapped_callback = functools.partial(callback, uuid)
 
-        await self.start_notify(uuid, lambda *args: None)
-
         self._listen_to_packets_task = asyncio.create_task(self.listen_for_packets(wrapped_callback))
         self._listen_to_packets_task.add_done_callback(self.handle_follow_file_task_done)
+
+        await self.start_notify(uuid, lambda *args: None)
 
         # await self.start_notify(uuid, callback)
         # if sys.platform == "linux" or sys.platform == "linux2":
